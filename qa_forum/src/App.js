0b45adfd1e7b7c45347c1f9a52a0216a3264a935
Sakshi Gadegaonkar <sgadegaonkar@gmail.com>
2021-09-29 14:24:13 +0530

    Initial commit

---
+++ qa_forum/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import { Counter } from './features/counter/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">Hello let's build Quora Clone
+
+    </div>
+  );
+}
+
+export default App;
